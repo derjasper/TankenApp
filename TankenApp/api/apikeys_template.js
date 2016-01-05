a07bf9ebb67f2d84b232d6a4ad2db31141d3fc36
Jasper Nalbach <jasper@bluetreehouse.de>
2016-01-05 11:59:16 +0100

    fixed keys

---
+++ TankenApp/api/apikeys_template.js
@@ -0,0 +1,15 @@
+// rename this file to apikeys.js and fill in the api keys
+var apikey = {
+    tankerkoenig: {
+        key: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", // get your key at https://creativecommons.tankerkoenig.de/
+        url: "https://creativecommons.tankerkoenig.de/"
+    },
+    mygasfeed: {
+        key: "xxxxxxxxxx", // get your key at http://www.mygasfeed.com/keys/intro
+        url: "http://api.mygasfeed.com/"
+    },
+    spritpreisrechner: {
+        key: "", // no api key needed
+        url: "http://www.spritpreisrechner.at/espritmap-app/GasStationServlet"
+    }
+};
